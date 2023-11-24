8e53c98b8e36ae8d32aedc8f1e67063cdda23f4e
Kristina Popova <kristina_14_09@vk.com>
2023-11-24 10:44:08 +0300

    Добавьте файлы проекта.

---
+++ labb4/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace labb4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ansver_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(textbox2.Text, out int n))
+            {
+                MessageBox.Show("Введите трехзначное число");
+
+            }
+            int firstDitit = n / 100;
+            int secondDigit = n / 10 % 10;
+            if (firstDitit > secondDigit)
+            {
+                textbox3.Text=("Первая цифра больше второй").ToString();
+            }
+            else if (firstDitit < secondDigit)
+            {
+                textbox3.Text = ("Вторая цифра больше первой").ToString();
+            }
+            else
+            {
+                textbox3.Text = ("Первая и вторая цифры равны").ToString();
+            }
+        }
+    }
+}
